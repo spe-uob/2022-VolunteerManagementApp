54bc9b963086538b51f637db79b983691e88a906
Abicem <abicem@Abicems-MacBook-Pro.local>
2023-02-09 15:56:34 +0000

    made full app branch mergeable with main

--- api/vueapp/src/components/myButton.vue
+++ api/vueapp/src/components/myButton.vue
@@ -1,10 +1,12 @@
 <template>
-    <button>aButton</button>
+    <!-- <button :style="{left: left + 'px'}"
+            v-bind:class="{'dark-button': clicked, 'light-button': !clicked}"
+            v-on:click="clicked=!clicked">{{ label }}</button> -->
+    <button :style="{ backgroundColor: selected ? '#6B818C' : '#ffffff', left: left}" class="custom-button" @click="$emit('click', $event)">{{ label }}</button>
 </template> 
 
 <script>
-//import $ from 'jquery';
-//import { response } from 'express';
+//may be good to only change background with :style to desired colour after clicked -> only one button change css
     export default {
         name: 'myButton',
         //props: [action],
@@ -13,60 +15,51 @@
         
         data(){
             return {
-
+                clicked: false,
             }
         },
-        props: ['action', 'activeResident','title', 'requirements','help_types'],
+        props: ['label', 'left', 'selected'],
         computed: {
 
         },
         methods: {
-        // addNewAction: function () {
-        //     let action = {
-        //         "resident": 1,
-        //         "volunteers_needed": 1,
-        //         "requested_datetime": "2021-04-21 06:05",
-        //         "interested_volunteers": [],
-        //         "assigned_voluneteers": [],
-        //         "action_status": "1",
-        //         "action_priority": "2",
-        //         "public_description":"",
-        //         "private_description":"",
-        //         "help_type":2,
-        //         "created_datetime":"",
-        //         "total_time_taken":""
-        //     }
-        //     const csrftoken = this.getCookie('csrftoken');
-        //     $.ajax({
-        //         url: "http://localhost:8000/" + "api/actions",
-        //     beforeSend: function(xhr) {
-        //         xhr.setRequestHeader('X-CSRFToken', csrftoken)
-        //     },
-        //     method: "POST",
-        //     type:"POST",
-        //     contentType:'application/json',
-        //     data: action,
-        //     success: (response) => {
-        //         this.$emit('new-action', response)
-        //     },
-        //     error: (err) => {
-        //         console.error(err)
-        //     }
-        //     })
-        // },
     }, 
 }
 </script>
 
 <style scoped>
-.button {
-  background-color: #4CAF50; /* Green */
-  border: none;
-  color: white;
-  padding: 15px 32px;
-  text-align: center;
-  text-decoration: none;
-  display: inline-block;
-  font-size: 16px;
+.dark-button{
+    color: white;
+    border: #ffffff;
+    position: absolute;
+    width: 120px;
+    height: 37px;
+    top: 118px;
+
+    background: #6B818C;
+    border-radius: 5px;
+
+    /* font-family: Inter;
+    font-style: normal;
+    font-weight: 400;
+    font-size: 16px; */
 }
+
+.custom-button{
+    color: black;
+    border: #ffffff;
+    position: absolute;
+    width: 120px;
+    height: 37px;
+    top: 118px;
+
+    background: #ffffff;
+    border-radius: 5px; 
+/* 
+    font-family: Inter;
+    font-style: normal;
+    font-weight: 400;
+    font-size: 16px; */
+}
+
 </style>
