c4bca6a381080b984eb4f6029c34974cecd15fdc
Abicem <abicem@Abicems-MacBook-Pro.local>
2023-02-13 20:27:21 +0000

    implemenented start call UX, lists and forms push to api

---
+++ api/vueapp/src/components/Start_Call.vue
@@ -0,0 +1,82 @@
+<template>
+    <div>
+        <button class="back-button" @click="back">&lt;</button>
+        <h1 class="call-title">
+          Select Resident
+        </h1>
+        <div>
+            <myButton v-for="(item, index) in buttons" :key="index" :label="item.label" :left="item.left" @click.native="selectButton(index)" :selected="item.selected" style="top: 25%; width: 150px;"/>
+        </div>
+        <ResidentList v-if="!NewResident"/>
+        <newResidentForm class="comp-container" v-if="NewResident"/>
+    </div>
+</template>
+
+<script>
+import ResidentList from './ResidentList.vue'
+import myButton from './myButton.vue'
+import newResidentForm from './newResidentForm.vue'
+export default {
+    name: 'Start_Call',
+    components: {
+        myButton,
+        ResidentList,
+        newResidentForm
+    },
+    data(){
+        return {
+            NewResident: false,
+            buttons: [
+                    {label: 'Existing Resident', left: '273px', selected: true},
+                    {label: 'New Resident', left: '426px', selected: false},
+                ],
+        }
+    },
+    props: [],
+    methods: {
+        back(){
+            this.$router.push("/All Activity")
+        },
+        selectButton(index) {
+                this.buttons.forEach((item, i) => {
+                    item.selected = (i === index)
+                })
+                this.NewResident = !this.NewResident
+            },
+        }
+}
+</script>
+
+<style>
+.back-button {
+  background-color: transparent;
+  border: none;
+  color: black;
+  font-size: 20px;
+  letter-spacing: -5px;
+  cursor: pointer;
+  position: absolute;
+  left: 15%;
+  top: 12%;
+  display: inline-block;
+}
+.call-title{
+    margin-left: 15%;
+    margin-top: -5%;
+}
+
+.comp-container{
+    box-sizing: border-box;
+
+    position: absolute;
+    width: 1229px;
+    height: 500px;
+    left: 100px;
+    top: 300px;
+
+    background: #F2F2F2;
+    border: 1px solid #DFDFDF;
+    border-radius: 5px;
+}
+
+</style>
--- api/vueapp/src/main.js
+++ api/vueapp/src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 import actionTable from './components/actionTable'
 import All_Activity from './components/All_Activity'
 import referralTable from './components/referralTable'
+import Start_Call from './components/Start_Call'
+import residentPage from './components/residentPage'
 import VueRouter from 'vue-router'
 import { loremIpsum } from "lorem-ipsum"
 import BootstrapVue from 'bootstrap-vue'
@@ -18,7 +20,9 @@ const routes = [
   { path: '/vue-test', component: App},
   { path: '/Actions', component: actionTable},
   { path: '/Referrals', component: referralTable},
-  { path: '/All Activity', component: All_Activity}
+  { path: '/All Activity', component: All_Activity},
+  { path: '/Start_Call', component: Start_Call},
+  { path: '/add/:id', component: residentPage}
 ]
 
 const router = new VueRouter({
@@ -28,13 +32,3 @@ const router = new VueRouter({
     router,
     render: h => h(App),
   }).$mount('#app')
-
-  // new Vue({
-  //     router,
-  //     render: h => h(one)
-  //   }).$mount('#one')
-
-  // new Vue({
-  //     router,
-  //     render: h => h(One),
-  // }).$mount('#app')
