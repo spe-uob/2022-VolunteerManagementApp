7df9a718bab8922821c3d4138d09e6ebc83f6243
Ismael Bencharef <66883069+IshShogun@users.noreply.github.com>
2023-04-22 14:10:40 +0100

    integrate api to action_page and adjust colours

--- Application/api/helloworldvue/src/components/action_page.vue
+++ Application/api/helloworldvue/src/components/action_page.vue
@@ -2,7 +2,7 @@
    <div class="parent-container">
         <div class="child-container">
             <div class="details-title">
-                <header>Action Details</header>
+                <header class="this-title">Action Details</header>
             </div>
             <div class="details">
                 <ul>
@@ -15,19 +15,19 @@
         </div>
         <div class="child-container">
             <div class="details-title">
-                <header>Assigned</header>
+                <header class="this-title">Assigned</header>
             </div>
             <div class="details">
                 <label for="selectedHelpType">Status:</label>
                 <select v-model="selectedHelpType">
-                <option v-for="helptype in helptypes" :key="helptype.id">{{ helptype.name }}</option>
+                <option v-for="helptype in helpTypes" :key="helptype.id">{{ helptype.name }}</option>
                 </select>
 
             </div>
         </div>
         <div class="footer-container">
             <div class="details-title">
-                <header>Assign Volunteers</header>
+                <header class="this-title">Assign Volunteers</header>
             </div>
             <div class="details">
                 <ul>
@@ -53,7 +53,7 @@ export default {
                 help_type: "Shopping",
                 Due: "20th March 2032"
             },
-            helptypes: [
+            helpTypes: [
                 { id: 1, name: "Pending volunteer interest" },
                 { id: 2, name: "Volunteer interest" },
                 { id: 3, name: "Volunteer assigned" },
@@ -73,7 +73,8 @@ export default {
                 {
                 name: "No One"
                 }
-            ]
+            ],
+            priority: ["High", "Medium", "Low"]
 
         }
     },
@@ -86,10 +87,13 @@ export default {
     // },
   },
     methods: {
-        getActions: async function () {
+            baseURL: function(){
+        return window.location.origin
+      },
+        getAction: async function () {
             const csrftoken = this.getCookie('csrftoken')
             const json = await $.ajax({
-                url: this.baseURL() + `/api/actions/${this.id}`,
+                url: this.baseURL() + `/api/actions/1`,
                 beforeSend: function (xhr) {
                 xhr.setRequestHeader('X-CSRFToken', csrftoken)
                 },
@@ -105,7 +109,7 @@ export default {
                 },
 
             }).catch((err) => {
-                console.err(JSON.stringify(err))
+                console.error(JSON.stringify(err))
             })
             console.log(JSON.stringify(json))
             return json;
@@ -211,7 +215,7 @@ export default {
     },
     async mounted(){
     let result = await this.getAction();
-    // console.log("GETACTIONS RESPONSE: " + JSON.stringify(response));
+    console.log("GETACTIONS RESPONSE: " + JSON.stringify(result));
     this.action = {
       id: result.id,
       resident: await this.getResidentByID(result.resident),
@@ -222,12 +226,16 @@ export default {
       priority: this.getPriorityByID(result.action_priority)
     };    
 
-    let response = await this.getVolunteers()
-    this.volunteers = response.result.map((obj) => {
+    let response = await this.getVolunteers();
+    console.log(response.results)
+    this.volunteers = response.results?.map((obj) => {
         return {
-            name: obj.first_name + " " + obj.last_name,
-        }
-    });
+        name: obj.first_name + " " + obj.last_name,
+    }}) || [];
+
+    console.log("this.volunteers: " + this.volunteers)
+    console.log("this.action: " + this.action)
+
   }
 }
 </script>
@@ -236,36 +244,54 @@ export default {
  .parent-container {
   display: flex;
   flex-wrap: wrap;
-  background-color: green;
+  background-color: white;
   width: 100%;
   height: 100%;
  }
  .child-container {
-    background-color: blue;
+    border-radius: 10px;
+    background-color: rgb(232, 232, 238);
+    /* border: 1px solid rgb(129, 120, 120); */
     flex-basis: 45%;
     box-shadow: -5px -1px 10px rgba(62, 62, 62, 0.134);
     padding: 10px;
     margin: 20px;
  }
  .details-title {
-    border: 1px solid black;
+    font: "Times New Roman";
+    border-top-right-radius: 10px;
+    border-top-left-radius: 10px;
+    background-color: rgb(211, 209, 209);
+    border: 1px solid rgb(89, 89, 89);
     padding: 5px;
     height: 10%;
     float: top;
  }
  .details {
+    font-family:"Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", "Liberation Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
+    border-bottom-left-radius: 10px;
+    border-bottom-right-radius: 10px;
+    border: 1px solid rgb(129, 120, 120);
     box-shadow: -5px -1px 10px rgba(62, 62, 62, 0.134);
-    background-color: aqua;
+    background-color: white;
+    padding: 20px;
  }
 
  .details-item {
     padding: 10px;
  }
 .footer-container {
+    border-radius: 10px;
     background-color: grey;
     align-self: flex-end;
     width: 100%;
     height: 20%;
     box-shadow: -5px -1px 10px rgba(62, 62, 62, 0.134);
+}
+
+.this-title {
+    font-family:"Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", "Liberation Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
+    font-weight: bold;
+    font-size: 100%;
 }
 </style>
